aba0c6de4aed3e274d6450d85ff93bd400323425
Erzhan Aldamatov <33819603+aldamatov@users.noreply.github.com>
2022-05-18 18:02:39 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./styles.css";
+
+function App() {
+  const arr = [1, 2, 3, 4, 5];
+  const [position, setPosition] = useState(-1);
+  const [flag, setFlag] = useState(false);
+  const handleClick = () => {
+    setFlag(true);
+  };
+  const handleMouseOver = (ind) => {
+    setPosition(ind);
+    setFlag(flag);
+  };
+  const handleMouseLeave = () => {
+    if (!flag) {
+      setPosition(-1);
+    }
+  };
+
+  return (
+    <div className="App">
+      {arr.map((item, i) => {
+        let active = position >= i ? "active" : "";
+        return (
+          <div
+            className={`box ${active}`}
+            onClick={handleClick}
+            onMouseOver={() => handleMouseOver(i)}
+            onMouseLeave={handleMouseLeave}
+          >
+            {item}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
